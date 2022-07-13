8ced3c0a0a3c2edb913d4fa954966e2d18f16f5d
Victor Felder <victor@draft.li>
2022-07-13 12:54:10 +0200

    feat: interactive editor

--- demo/main.js
+++ demo/main.js
@@ -1,12 +1,23 @@
 import {toHtml} from 'hast-util-to-html'
 import {doc, heading, paragraph, render} from 'glsf'
 
+const editor = document.getElementById('tree')
+const output = document.getElementById('output')
+
 const tree = doc([
   heading('Title', {level: 1}),
   heading('Subtitle', {level: 2}),
   paragraph('Welcome')
 ])
 
-const hast = render(tree)
+function renderHtml () {
+  const tree = JSON.parse(editor.value)
+  const hast = render(tree)
+  const html = toHtml(hast)
+  output.innerHTML = html
+}
 
-document.querySelector('body').innerHTML = toHtml(hast)
+editor.innerHTML = JSON.stringify(tree, null, 2)
+renderHtml()
+
+editor.addEventListener('input', renderHtml)
